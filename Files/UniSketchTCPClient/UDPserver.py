3c79c2ddeae7d47182d9eef46bb70284aa3df413
Kasper Skårhøj <kasperskaarhoj@gmail.com>
2020-04-26 21:59:32 +0200

    Manual and test script for device core "UDP CLient"

---
+++ Files/UniSketchTCPClient/UDPserver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import logging
+import socket
+
+log = logging.getLogger('udp_server')
+
+
+def udp_server(host='', port=1338):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    log.info("Listening on udp %s:%s" % (host, port))
+    s.bind((host, port))
+    while True:
+        (data, addr) = s.recvfrom(128*1024)
+        yield data
+
+
+FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)8s\t%(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT_CONS)
+
+for data in udp_server():
+    log.debug("%r" % (data,))
+
+
+# Thanks to https://gist.github.com/majek/1763628
